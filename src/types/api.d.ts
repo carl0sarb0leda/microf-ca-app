9287864cba6c32599e856859d4aea4d13a9b3775
carl0sarb0leda <carlosaepn@gmail.com>
2022-11-23 19:57:08 +1300

    Update api types

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,4 +1,13 @@
 export type LogInProps = {
-    userName: string;
-    userPassword: string;
-  };
+  userName: string;
+  userPassword: string;
+};
+
+export type ErrorResponse = {
+  httpStatusCode: number;
+  errorMessage: string;
+};
+
+export type SessionTokenResponse = {
+  sessionToken: string;
+};
