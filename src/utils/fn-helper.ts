f567454d146c4e244b161499b11ef23009514343
carl0sarb0leda <carlosaepn@gmail.com>
2023-04-03 14:07:21 +1200

    Update helper functions

--- src/utils/fn-helper.ts
+++ src/utils/fn-helper.ts
@@ -1,6 +1,15 @@
+import {redirect} from 'react-router-dom'
+
 export const formatName = (firstName: string, name?: string) => {
   if (name) {
-    return `${name} (${firstName})`;
+    return `${name} (${firstName})`
   }
-  return firstName;
-};
+  return firstName
+}
+
+export const customLoader = async () => {
+  //validation for gh-pages deployment
+  const pathname = window.location.pathname
+  if (pathname === '/microf-container-app/') return redirect('/')
+  return null
+}
